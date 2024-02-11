f4a2fd00895dec8fe94dac1949e3a1556f6c3110
walid209 <walidkhataoui1@gmail.com>
2024-02-11 13:11:46 -0800

    test

---
+++ warmupTest.js
@@ -0,0 +1,93 @@
+
+//make a function that return the first character from a string
+
+
+function theFirstCharacter(string){
+ // your code here
+}
+var carName = Volvo 
+
+//2
+
+var length = 16;
+// number
+
+var lastName = "Johnson"; 
+//string
+
+var isGreaterThan10 = length > 10 
+//boolean 
+
+
+//3
+
+function square1 (x) { 
+    return x * x  
+    } 
+
+function square2 (x) {
+    return x * x  
+    }
+
+function square3 (x) { 
+        return x * x
+    }
+
+//4)
+
+function largest(f , h) {
+  if ( f > h )
+    return f 
+  else 
+    return h 
+}
+
+
+//5)
+
+function add(s, d) {
+    return s + d
+
+}
+
+
+//6)
+
+function subtract (a, b) {
+    return a - b
+
+}
+
+//7 
+
+var Operator = '+'
+
+var x = 20
+var y = 15
+
+function addOrSub (opera) {
+if ( opera === Operator ) 
+{
+    console.log(add(x , y ))
+     return add ( x, y) }
+    
+ else    
+    console.log(subtract(x , y))
+    return subtract ( x , y)
+    
+
+}
+
+
+//8 
+
+function divide(num1 , num2 ) {
+
+    return num1 / num2
+   
+}
+
+function multiply(num1 , num2 ) {
+
+    return num1 * num2    
+}
